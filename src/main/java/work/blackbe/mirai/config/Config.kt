fa73bf483f24efb44608ca6c8449e268b9474da4
McPlus <mcplusapp@gmail.com>
2023-01-20 03:04:24 +0800

    feat: 新增target占位符 修改配置文件

--- src/main/java/work/blackbe/mirai/config/Config.kt
+++ src/main/java/work/blackbe/mirai/config/Config.kt
@@ -3,10 +3,9 @@ package work.blackbe.mirai.config
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 
-
 object Config : AutoSavePluginConfig("Config") {
     val expression by value("^查云黑(.+)$")
-    val response by value("QQ \${qq} 存在违规行为\n" +
+    val response by value("\${target} 存在违规行为\n" +
             "================\n" +
             "玩家ID: \${player_name}\n" +
             "UUID: \${uuid}\n" +
